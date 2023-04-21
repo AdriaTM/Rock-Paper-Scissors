bfff73384dcb2ff002385d3a7df84c135ef9bb1e
AdriaTM <adriatolra@gmail.com>
2023-04-21 16:35:10 +0200

    Creates a div in the DOM showing the result

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-function getComputerChoice() {
-    const myArray = ["Rock", "Paper", "Scissors"];
-    return myArray[Math.floor(Math.random() * myArray.length)];
-}
-
 const rockChoice = document.getElementById("rockBtn");
 const paperChoice = document.getElementById("paperBtn");
 const scissorsChoice = document.getElementById("scissorsBtn");
+const scoreboard = document.querySelector('.scoreboard');
+const div = document.createElement('div');
 
 
 rockChoice.addEventListener('click', () => handleClick("rock"));
 paperChoice.addEventListener('click', () => handleClick("paper"));
 scissorsChoice.addEventListener('click', () => handleClick("scissors"));
 
+function getComputerChoice() {
+    const myArray = ["Rock", "Paper", "Scissors"];
+    return myArray[Math.floor(Math.random() * myArray.length)];
+}
+
+
 function handleClick(playerSelection) {
     const computerSelection = getComputerChoice();
     let result = playRound(playerSelection, computerSelection);
+    div.classList.add('result');
+    div.textContent = result;
+    scoreboard.appendChild(div);
 }
 
 
