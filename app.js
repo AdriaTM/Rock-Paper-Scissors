12eac7c6d82e155422b001c67037a4d0299b8b26
AdriaTM <adriatolra@gmail.com>
2023-04-21 18:53:09 +0200

    Updated the logic of the game

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+let computer = 0;
+let player = 0;
+
 const rockChoice = document.getElementById("rockBtn");
 const paperChoice = document.getElementById("paperBtn");
 const scissorsChoice = document.getElementById("scissorsBtn");
@@ -5,65 +8,50 @@ const scoreboard = document.querySelector('.scoreboard');
 const div = document.createElement('div');
 const score = document.createElement('div');
 
-rockChoice.addEventListener('click', () => handleClick("rock"));
-paperChoice.addEventListener('click', () => handleClick("paper"));
-scissorsChoice.addEventListener('click', () => handleClick("scissors"));
+score.classList.add('score');
+score.textContent = "Player: " + player + " Computer: " + computer;
+scoreboard.appendChild(score);
+
+rockChoice.addEventListener('click', () => handleClick('ROCK'));
+paperChoice.addEventListener('click', () => handleClick('PAPER'));
+scissorsChoice.addEventListener('click', () => handleClick('SCISSORS'));
 
 function getComputerChoice() {
-    const myArray = ["Rock", "Paper", "Scissors"];
-    return myArray[Math.floor(Math.random() * myArray.length)];
+    let random = Math.floor(Math.random() * 3);
+    switch (random) {
+        case 0:
+            return 'ROCK';
+        case 1:
+            return 'PAPER';
+        case 2:
+            return 'SCISSORS';
+    }
 }
 
 function handleClick(playerSelection) {
     const computerSelection = getComputerChoice();
     let result = playRound(playerSelection, computerSelection);
     div.classList.add('result');
-    div.textContent = result;
+    div.textContent = "PLAYER SELECTS: " + playerSelection + " COMPUTER SELECTS: " + computerSelection + " = " + result;
     scoreboard.appendChild(div);
 }
 
 function playRound(playerSelection, computerSelection) {
     let result = "";
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
     if (playerSelection === computerSelection) {
-        result = "draw";
+        result = "IT'S A DRAW!";
     }
-    else {
-        if (computerSelection == "paper") {
-            if (playerSelection == "rock") {
-                result = "computer";
-            }
-            else {
-                result = "player";
-            }
-        }
-        else if (computerSelection == "rock") {
-            if (playerSelection == "paper") {
-                result = "player";
-            }
-            else {
-                result = "computer";
-            }
-        }
-        else {
-            if (playerSelection == "rock") {
-                result = "player";
-            }
-            else {
-                result = "computer"
-            }
-        }
+    if ((playerSelection === 'ROCK' && computerSelection === 'SCISSORS') ||
+        (playerSelection === 'PAPER' && computerSelection === 'ROCK') ||
+        (playerSelection === 'SCISSORS' && computerSelection === 'PAPER')) {
+        player++;
+        result = 'PLAYER WINS!';
+    }
+    if ((computerSelection === 'ROCK' && playerSelection === 'SCISSORS') ||
+        (computerSelection === 'PAPER' && playerSelection === 'ROCK') ||
+        (computerSelection === 'SCISSORS' && playerSelection === 'PAPER')) {
+        computer++;
+        result = 'COMPUTER WINS!';
     }
     return result;
 }
-
-function game() {
-    let computer = 0;
-    let player = 0;
-    score.classList.add('score');
-    score.textContent = player + " " + computer;
-    scoreboard.appendChild(score);
-}
-
-game();
